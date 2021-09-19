5ec4271307c933d017bfc68d880abc2c00ad97e9
Nardin Lachowski <nardin@live.com>
2021-09-19 16:04:58 -0400

    added the main js file into which the Game and GameView classes are imported with the selection of the board-tile and which functions run on tile click

---
+++ js/main.js
@@ -0,0 +1,19 @@
+import { Game } from './Game.js';
+import { GameView } from './GameView.js';
+
+let game = new Game();
+let gameView = new GameView();
+
+
+let tiles = document.querySelectorAll(".board-tile");
+tiles.forEach((tile) => {
+    tile.addEventListener("click", () => {
+        onTileClick(tile.dataset.index);
+    })
+})
+
+
+function onTileClick(i){
+    game.makeMove(i);
+    gameView.updateBoard(game);
+}
