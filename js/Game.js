112b434ce8c6a9391ff61fb2d918905193e7801e
Nardin Lachowski <nardin@live.com>
2021-09-19 16:02:45 -0400

    adding new game class which includes the logic when actions are taken by user, such as making a move, checking win condition, ending game, determining turn order

---
+++ js/Game.js
@@ -0,0 +1,63 @@
+export default class Game {
+
+    constructor(){
+        this.turn = "X";
+        this.board = new Array(9).fill(null);
+    }
+
+    nextTurn(){
+        if(this.turn == "X") {
+            this.turn = "O";
+        } else {
+            this.turn = "X";
+        }
+    }
+
+    makeMove(i){
+
+        if (this.endofGame) {
+            return;
+        }
+
+        if (this.board[i]) {
+            return;
+        }
+        this.board[i] = this.turn;
+        let winningCombination = this.checkWin();
+        if(!winningCombination){
+            this.nextTurn();
+        }
+    }
+
+    checkWin(){
+        const winningCombos = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [6,4,2]
+        ]
+        for(const combo of winningCombos) {
+            const [a,b,c] = combo;
+
+            if (this.board[a] &&
+                (this.board[a] === this.board[b] && this.board[a] === this.board[c])) {
+                    return combo;
+                }
+        }
+        return null;
+    }
+
+    endofGame(){
+        let winningCombination = this.winningCombos;
+        if(winningCombination){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
