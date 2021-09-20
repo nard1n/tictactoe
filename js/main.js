85ffa1fbd90ac4c6ec6c5d3938ed91bc687b585b
Nardin Lachowski <nardin@live.com>
2021-09-20 14:47:52 -0400

    updated updateboard function name to board when called here

--- js/main.js
+++ js/main.js
@@ -6,22 +6,22 @@ let gameView = new GameView();
 
 document.querySelector(".restart").addEventListener("click", () => {
     onRestartClick();
-})
+});
 
-let tiles = document.querySelectorAll(".board-tile");
-tiles.forEach((tile) => {
+document.querySelectorAll(".board-tile").forEach((tile) => {
     tile.addEventListener("click", () => {
         onTileClick(tile.dataset.index);
     });
 });
 
-
 function onTileClick(i){
     game.makeMove(i);
-    gameView.updateBoard(game);
+    gameView.update(game);
 }
 
 function onRestartClick() {
     game = new Game();
-    gameView.updateBoard(game);
+    gameView.update(game);
 }
+
+gameView.update(game);
