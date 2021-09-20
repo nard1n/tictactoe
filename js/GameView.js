198bf3a623b5cd1b127735288b188668fb78b185
Nardin Lachowski <nardin@live.com>
2021-09-20 10:49:12 -0400

    adding updeboard function

--- js/GameView.js
+++ js/GameView.js
@@ -1,11 +1,14 @@
 export default class GameView {
 
+    update(game) {
+        this.updateBoard(game);
+    }
+
     updateBoard(game){
         for (let i = 0; i < game.board.length; i++){
             const tile = document.querySelector(`.board-tile[data-index="${i}"]`);
             console.log(tile);
             tile.textContent = game.board[i];
         };
-
     };
 }
