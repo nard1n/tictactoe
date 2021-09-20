8c4f38dae8c97ec6f94062c31441952a164e7f83
Nardin Lachowski <nardin@live.com>
2021-09-20 12:36:50 -0400

    added logic to update player state to active when turn changes and corrected typo

--- js/GameView.js
+++ js/GameView.js
@@ -1,14 +1,24 @@
 export default class GameView {
 
-    update(game) {
-        this.updateBoard(game);
-    }
-
     updateBoard(game){
+        this.updateTurn(game);
         for (let i = 0; i < game.board.length; i++){
-            const tile = document.querySelector(`.board-tile[data-index="${i}"]`);
-            console.log(tile);
+            const tile = document.querySelector(`.board-tile [data-index="${i}"]`);
             tile.textContent = game.board[i];
         };
     };
+
+    updateTurn(game){
+        let playerX = document.querySelector(".player-x");
+        let playerO = document.querOSelector(".player-o");
+        playerX.classList.remove("active");
+        playerY.classList.remove("active");
+
+        if(game.turn == 'X') {
+            playerX.classList.add('active');
+
+        } else {
+            playerO.classList.add('active');
+        }
+    }
 }
