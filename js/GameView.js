b646176659470fb001f50be6d1988eb63e1e6415
Nardin Lachowski <nardin@live.com>
2021-09-20 14:46:02 -0400

    added winner tile color to be added when win condition is met and removed when game is restarted

--- js/GameView.js
+++ js/GameView.js
@@ -1,13 +1,22 @@
 export default class GameView {
 
-    updateBoard(game){
+    update(game){
         this.updateTurn(game);
+
+        const winningCombination = game.checkWin();
+        
         for (let i = 0; i < game.board.length; i++){
-            const tile = document.querySelector(`.board-tile [data-index="${i}"]`);   
+            const tile = document.querySelector(`.board-tile [data-index="${i}"]`);  
+            
+            tile.classList.remove("tile-winner");
 
             let tileType = game.board[i] == 'X' ? "tile-x" : "tile-o";
 
             tile.innerHTML = `<span class="${tileType}">${game.board[i] ? game.board[i] : ""}</span>`
+
+            if(winningCombination && winningCombination.includes(i)){
+                tile.classList.add("tile-winner");
+            }
         };
     };
 
