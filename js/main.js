07d5dbec0fa0865e5362fa234d1f5b11c81f940f
Nardin Lachowski <nardin@live.com>
2021-09-20 10:48:19 -0400

    added event listener to restart button

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ import GameView from './GameView.js';
 let game = new Game();
 let gameView = new GameView();
 
+document.querySelector(".restart").addEventListener("click", () => {
+    onRestartClick();
+})
 
 let tiles = document.querySelectorAll(".board-tile");
 tiles.forEach((tile) => {
@@ -15,5 +18,10 @@ tiles.forEach((tile) => {
 
 function onTileClick(i){
     game.makeMove(i);
+    gameView.updateBoard(game);
+}
+
+function onRestartClick() {
+    game = new Game();
     gameView.updateBoard(game);
 }
