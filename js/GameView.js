77e1d1145c539ef716f2b74b70bab25a62d7a4b8
Nardin Lachowski <nardin@live.com>
2021-09-28 18:25:00 -0400

    corrected tile index selector syntax

--- js/GameView.js
+++ js/GameView.js
@@ -6,7 +6,7 @@ export default class GameView {
         const winningCombination = game.checkWin();
         
         for (let i = 0; i < game.board.length; i++){
-            const tile = document.querySelector(`.board-tile [data-index="${i}"]`);  
+            const tile = document.querySelector(`.board-tile[data-index="${i}"]`);  
             
             tile.classList.remove("tile-winner");
 
